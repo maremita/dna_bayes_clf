3205bf0f814eaa22bc30ca3433af7fbe24608363
Amine M. Remita <remita.mohamed_amine@courrier.uqam.ca>
2019-02-16 00:10:07 -0500

    save cv results to json

--- eval_complete_seqs.py
+++ eval_complete_seqs.py
@@ -5,6 +5,7 @@ from src import kmers
 from src import bayes
 from src import utils
 
+import json
 from pprint import pprint
 
 import numpy as np
@@ -15,10 +16,6 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 
-#import json
-#import joblib
-
-# import sys
 
 __author__ = "amine"
 __version__ = "0.1"
@@ -61,14 +58,14 @@ def kmer_dataset_construction(k_main=9, k_estim=3,
     return all_words_data, all_backs_data
 
 
-def clf_evaluation(classifiers, X, y, cv_iter=10, scoring=["f1_weighted"],
+def clf_evaluation(classifiers, X, y, cv_iter=5, scoring=["f1_weighted"],
         random_state=None, verbose=False):
 
     scores = dict()
     
     if verbose: print("\nCross-Validation step")
 
-    skf = StratifiedKFold(n_splits=cv_iter, shuffle=False, random_state=random_state)
+    skf = StratifiedKFold(n_splits=cv_iter, shuffle=True, random_state=random_state)
 
     for clf_ind in classifiers:
         classifier, params, clf_dscp = classifiers[clf_ind]
@@ -76,9 +73,22 @@ def clf_evaluation(classifiers, X, y, cv_iter=10, scoring=["f1_weighted"],
 
         if verbose: print("\nEvaluating {}".format(clf_dscp))
 
-        scores[clf_dscp] = cross_validate(clf, X, y, cv=skf, scoring=scoring)
+        scores_tmp = cross_validate(clf, X, y, cv=skf, scoring=scoring, return_train_score=True)
+        scores[clf_dscp] = ndarrays_tolists(scores_tmp)
 
     return scores
+
+def ndarrays_tolists(obj):
+    new_obj = dict()
+
+    for key in obj:
+        if isinstance(obj[key], np.ndarray):
+            new_obj[key] = obj[key].tolist()
+
+        else:
+            new_obj[key] = obj[key]
+
+    return new_obj
 
 
 if __name__ == "__main__":
@@ -89,9 +99,9 @@ if __name__ == "__main__":
     verbose = True
     priors="uniform"
  
-    scores_file = "results/viruses/HBV01.json"
-    cls_file = "data/viruses/HBV01/class.csv"
-    seq_file = "data/viruses/HBV01/data.fa"
+    scores_file = "results/viruses/HIV02.json"
+    cls_file = "data/viruses/HIV02/class.csv"
+    seq_file = "data/viruses/HIV02/data.fa"
 
     if verbose: print("\nDataset construction step")
     seq_cv_X, seq_cv_y, seq_estim_X, seq_estim_y = seq_dataset_construction(seq_file, cls_file, k_main, k_estim)
@@ -107,21 +117,23 @@ if __name__ == "__main__":
 
     classifiers = {
             0: [bayes.MLE_MultinomialNB, {'priors':priors}, "MLE Multinom NBayes"],
-            #1: [bayes.Bayesian_MultinomialNB, {'priors':priors, 'alpha':1e-10}, "Bayesian Multinom NBayes with alpha=1e-10"],
-            #2: [bayes.Bayesian_MultinomialNB, {'priors':priors, 'alpha':1}, "Bayesian Multinom NBayes with alpha=1"],
+            1: [bayes.Bayesian_MultinomialNB, {'priors':priors, 'alpha':1e-10}, "Bayesian Multinom NBayes with alpha=1e-10"],
+            2: [bayes.Bayesian_MultinomialNB, {'priors':priors, 'alpha':1}, "Bayesian Multinom NBayes with alpha=1"],
             3: [bayes.Bayesian_MultinomialNB, {'priors':priors, 'alpha':a_mnom, 'alpha_classes':a_mnom_y}, "Bayesian Multinom NBayes with estimated alpha"],
 
-            #4: [bayes.MLE_MarkovModel, {'priors':priors}, "MLE Markov model"],
-            #5: [bayes.Bayesian_MarkovModel, {'priors':priors, 'alpha':1e-10}, "Bayesian Markov model with alpha=1e-10"],
-            #6: [bayes.Bayesian_MarkovModel, {'priors':priors, 'alpha':a_mkov, 'alpha_classes':a_mkov_y}, "Bayesian Markov model with estimated alpha"],
+            4: [bayes.MLE_MarkovModel, {'priors':priors}, "MLE Markov model"],
+            5: [bayes.Bayesian_MarkovModel, {'priors':priors, 'alpha':1e-10}, "Bayesian Markov model with alpha=1e-10"],
+            6: [bayes.Bayesian_MarkovModel, {'priors':priors, 'alpha':a_mkov, 'alpha_classes':a_mkov_y}, "Bayesian Markov model with estimated alpha"],
 
-            #7: [GaussianNB, {'priors':priors}, "Gaussian Naibe Bayes"],
-            #8: [LogisticRegression, {'multi_class':'multinomial', 'solver':'lbfgs', 'max_iter':500}, "Logistic Regression"],
-            #9: [SVC, {'C':1, 'kernel':"linear"}, "Linear SVC"]
+            7: [GaussianNB, {}, "Gaussian Naibe Bayes"],
+            8: [LogisticRegression, {'multi_class':'multinomial', 'solver':'lbfgs', 'max_iter':100}, "Logistic Regression"],
+            9: [SVC, {'C':1, 'kernel':"linear"}, "Linear SVC"]
            }
 
-    scores = clf_evaluation(classifiers, seq_cv_X, seq_cv_y, cv_iter=10, scoring=["f1_weighted"], random_state=rs, verbose=verbose)
-    print(scores)
+    scores = clf_evaluation(classifiers, seq_cv_X, seq_cv_y, cv_iter=5,
+            scoring=["f1_weighted"], random_state=rs, verbose=verbose)
 
-    #with open(scores_file ,"w") as fh_out: 
-    #    json.dump(scores, fh_out, indent=2) 
+    #print(scores)
+
+    with open(scores_file ,"w") as fh_out: 
+        json.dump(scores, fh_out, indent=2) 
