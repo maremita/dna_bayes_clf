e9ad40b9bcc849d3454706a9a6413297ebb81c77
Amine M. Remita <remita.amine@courrier.uqam.ca>
2019-06-29 19:27:18 -0400

    update and doc merge_k_jsons.py

--- scripts/merge_k_jsons.py
+++ scripts/merge_k_jsons.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
-from os import listdir
-from os.path import isfile, join
+from os.path import isfile
 import sys
 import json
 from collections import defaultdict
@@ -10,30 +9,48 @@ from glob import glob
 
 if __name__ == "__main__":
     """
+    Command line:
+
     merge_k_jsons.py 
-    ~/Projects/Thesis/dna_bayes_clf/results/viruses/wcb_2019/graham/HIV01_FT_250 
+    /Users/remita/Projects/Thesis/dna_bayes_clf/results/viruses/wcb_2019/graham/HIV01_FT_250 
     ~/Projects/Thesis/dna_bayes_clf/results/viruses/wcb_2019/graham/HIV01_FT_250/HIV01_FT_250.json
+    
+    The path of input files needs to be absolute
+
+    If the path does not end by a "/", the script add a */* to get all
+    the folders that match this pattern, otherwise (ends with "/"), 
+    it lists only the files in this file
+
     """
 
     k_scores = defaultdict(dict)
     final_scores = defaultdict(dict)
+    
+    if len(sys.argv) < 2:
+        sys.exit()
 
     my_path = sys.argv[1]
-    #prefix = sys.argv[2]
-    output_file = sys.argv[2]
+    output_file = None
+
+    # output file
+    if len(sys.argv) == 3:
+        output_file = sys.argv[2]
+    
+    if output_file is None:
+        if my_path.endswith("/"):
+            output_file = my_path.rstrip("/")+".json"
+        else:
+            output_file = my_path+".json"
 
     # Get files
-    #files= [f for f in listdir(my_path) if isfile(join(my_path, f)) and 
-    #        f.endswith(".json") and f.startswith(prefix)]
-
     suffix = "*/*"
     if my_path.endswith("/"):
         suffix = "/*"
 
-    files = [f for f in  glob(my_path+suffix) if isfile(f) ]
+    files = [f for f in  glob(my_path+suffix) if isfile(f) and
+            f.endswith(".json") ]
 
     for one_file in files:
-        #scores = json.load(open(join(my_path, one_file), "r"))
         scores = json.load(open(one_file, "r"))
 
         for clf_dscp in scores:
